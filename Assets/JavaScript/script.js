157b93eecb24f02caa284173d5e95c0a25c9c73f
Shini <halimshini@hotmail.com>
2023-03-13 00:10:36 -0400

    get items from localstorage fix

--- Assets/JavaScript/script.js
+++ Assets/JavaScript/script.js
@@ -1,34 +1,54 @@
 //Dependencies
-var timeBlock= $('.time-block');
-var present= $('.present');
-var future= $('.future');
-var past= $('.past');
-var description= $('.description');
-var save= $('.saveBtn');
-var currentDay= $('currentDay');  
+var timeBlock = $('.time-block');
+
+var save = $('.saveBtn');
+var currentDay = $('currentDay');
 
 //Current Day
 currentDay = dayjs();
-  $('#currentDay').text(currentDay.format('MMM D, YYYY'));
+$(currentDay).text(currentDay.format('MMM D, YYYY'));
 $(document).ready(function () {
-    // Save Button click listener 
-    $(".saveBtn").on("click", function () {
-        
-        var text = $(this).siblings(".description").val();
-        var time = $(this).parent().attr("id");
+  
+  $(save).on("click", function () {
 
-        // Save text in local storage
-        localStorage.setItem(time, text);
+    var text = $(this).siblings(".description").val();
+    var time = $(this).parent().attr("id");
+
+    // Save text in local storage
+    localStorage.setItem(time, text);
+  })
+
+  function time() {
+    var currentHour = dayjs().hour();
+    $(timeBlock).each(function () {
+      var blockHour = parseInt($(this).attr("id").split("hour")[1]);
+
+      if (blockHour < currentHour) {
+        $(this).removeClass("future");
+        $(this).removeClass("present");
+        $(this).addClass("past");
+      }
+      else if (blockHour === currentHour) {
+        $(this).removeClass("past");
+        $(this).removeClass("future");
+        $(this).addClass("present");
+      }
+      else {
+        $(this).removeClass("present");
+        $(this).removeClass("past");
+        $(this).addClass("future");
+      }
     })
-   // Get items from localStorage 
-    $("#hour-8 .description").val(localStorage.getItem("hour-8"));
-    $("#hour-9 .description").val(localStorage.getItem("hour-9"));
-    $("#hour-10 .description").val(localStorage.getItem("hour-10"));
-    $("#hour-11 .description").val(localStorage.getItem("hour-11"));
-    $("#hour-12 .description").val(localStorage.getItem("hour-12"));
-    $("#hour-13 .description").val(localStorage.getItem("hour-13"));
-    $("#hour-14 .description").val(localStorage.getItem("hour-14"));
-    $("#hour-15 .description").val(localStorage.getItem("hour-15"));
-    $("#hour-16 .description").val(localStorage.getItem("hour-16"));
-    $("#hour-17 .description").val(localStorage.getItem("hour-17"));
+  }
+
+  // Get items from localStorage 
+  $("#hour-9 .description").val(localStorage.getItem("hour-9"));
+  $("#hour-10 .description").val(localStorage.getItem("hour-10"));
+  $("#hour-11 .description").val(localStorage.getItem("hour-11"));
+  $("#hour-12 .description").val(localStorage.getItem("hour-12"));
+  $("#hour-13 .description").val(localStorage.getItem("hour-13"));
+  $("#hour-14 .description").val(localStorage.getItem("hour-14"));
+  $("#hour-15 .description").val(localStorage.getItem("hour-15"));
+
+  time();
 })
--- README.md
+++ README.md
@@ -1,9 +1,13 @@
 #  Work Day Scheduler
 
 ## Description
+A simple calendar application that allows the user to save events for each hour of the day. 
 
 ## Usage
-[Click Here](https://hshini.github.io/Work-Day-Scheduler/) to start. 
+
+[Click Here](https://hshini.github.io/Work-Day-Scheduler/) to open Work Day Scheduler. 
+![Work Day Scheduler](./Assets/Images/image.png)
+
 ## License
 Please refer to the LICENSE in the repo.
 
